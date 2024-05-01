3d4fa37ed0450df5bc19e3dc6077c0f6e032abc5
Rohan-tech11 <rohanreddy98765@gmail.com>
2024-04-30 23:39:39 -0400

    added-assets

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.jpg";
+import data from "./data";
 import "./navbar.css";
+import { IoIosColorPalette } from "react-icons/io";
 const Navbar = () => {
   return (
-    <navbar id="navbar">
-      <h1>navbar</h1>
-    </navbar>
+    <nav>
+      <div className="container nav__container">
+        <a href="index.html" className="nav__logo">
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className="nav__menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id="theme__icon">
+          <IoIosColorPalette />
+        </button>
+      </div>
+    </nav>
   );
 };
-
 export default Navbar;
