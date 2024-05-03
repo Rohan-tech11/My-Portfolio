ed60294e4ee5c50b4a18fb4bd2b5732606f8ca65
Rohan-tech11 <rohanreddy98765@gmail.com>
2024-05-03 00:04:25 -0400

    Added-HeaderComponent

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,43 @@
+import HeaderImage from "../../assets/header.jpg";
+import data from "./data";
+
 import "./header.css";
+
 const Header = () => {
   return (
     <header id="header">
-      <h1>header</h1>
+      <div className="container header__container">
+        <div className="header__profile" data-aos="fade-in">
+          <img src={HeaderImage} alt="Header Portait" />
+        </div>
+        <h3 data-aos="fade-up">Rohan Mogarala</h3>
+        <p data-aos="fade-up">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
+          deserunt temporibus recusandae adipisci, maiores minus aut facilis
+          debitis, laborum fugiat delectus tempora magnam ea minima iusto est.
+          Fuga, vel quae?
+        </p>
+        <div className="header__cta" data-aos="fade-up">
+          <a href="#contact" className="btn primary">
+            Let's Talk
+          </a>
+          <a href="#portfolio" className="btn light">
+            My Work
+          </a>
+        </div>
+        <div className="header__socials">
+          {data.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
     </header>
   );
 };
