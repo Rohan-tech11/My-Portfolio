490fc53ee8d597b433cc980c458c3654449d7cd0
Rohan-tech11 <rohanreddy98765@gmail.com>
2024-05-04 00:46:37 -0400

    added-services

--- src/sections/services/Services.jsx
+++ src/sections/services/Services.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./services.css";
+import data from "./data";
+import Card from "../../components/Card";
 const Services = () => {
   return (
-    <div>
-      <h1>services</h1>
-    </div>
+    <section id="services">
+      <h2>my serrvices</h2>
+      <p>
+        I will provide a comprehensive services below which will satisfy your
+        business requirements
+      </p>
+      <div className="container services__container">
+        {data.map((item) => (
+          <Card key={item.id} className="service light">
+            <div className="service__icon">{data.icon}</div>
+            <div className="service__title">
+              <h4>{item.title}</h4>
+              <p>{item.desc}</p>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </section>
   );
 };
 
